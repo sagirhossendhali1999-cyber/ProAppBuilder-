d1de6e852691a3685afdf7290bee60e824c61911
sagirhossendhali1999-cyber <sagirhossendhali1999@gmail.com>
2026-09-09 18:07:36 +0600

    Update SdkSettingsActivity.java

--- app/src/main/java/com/besome/sketch/SdkSettingsActivity.java
+++ app/src/main/java/com/besome/sketch/SdkSettingsActivity.java
@@ -4,7 +4,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import com.besome.sketch.R;
 
 public class SdkSettingsActivity extends AppCompatActivity {
     EditText etCompile, etTarget, etMin;
